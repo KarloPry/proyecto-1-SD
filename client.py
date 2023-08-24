3600bfb36d3a3dbd79b210a77082bcfcee66c13b
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-08-24 14:33:21 -0600

    feat: Terminado, el servidor envia de vuelta el archivo, falta limpieza de código

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 from encryption import Encryption
+from pprint import pprint
 llave = 22
 host = '127.0.0.1'
 port = 12345
@@ -10,7 +11,11 @@ with open(encrypted_image_path, "rb") as encrypted_image:
     client_socket.connect((host, port))
     client_socket.sendfile(encrypted_image)
     client_socket.shutdown(socket.SHUT_WR)
-    response = client_socket.recv(1024)
-    print(f"Respuesta del servidor: {response}")
+    with open("retrieved_image.png", "wb") as retrieved_image:
+        data = client_socket.recv(1024)
+        while data:
+            print(f"Recibiendo...")
+            retrieved_image.write(data)
+            data = client_socket.recv(1024)
     client_socket.close()
 
--- server.py
+++ server.py
@@ -1,34 +1,42 @@
 import socket
+
 llave = 22
+host = '127.0.0.1'
+port = 12345
+
 def main():
     print("Iniciando el servidor...")
-    host = '127.0.0.1'
-    port = 12345
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)
     print("Esperando mensajes entrantes")
-    f = open("server-image-rcv.png", "wb")
+    file_recieved = open("server-image-rcv.png", "wb")
     while True:
         client_socket, client_address = server_socket.accept()
         print("Conexion establecida desde:", client_address)
         data = client_socket.recv(1024)
         while data:
             print(f"Recibiendo...")
-            f.write(data)
+            file_recieved.write(data)
             data = client_socket.recv(1024)
-        f.write(data)
-        f.close()
+        file_recieved.write(data)
+        file_recieved.close()
         rf = open("server-image-rcv.png", "rb")
-        recieved_file = rf.read()
-        recieved_file = bytearray(recieved_file)
-        for index, value in enumerate(recieved_file):
-            recieved_file[index] = value ^ llave
+        recieved_file_content = rf.read()
+        recieved_file_content = bytearray(recieved_file_content)
+        for index, value in enumerate(recieved_file_content):
+            recieved_file_content[index] = value ^ llave
         image_generated = open("server-image-rtv.png", "wb")
-        image_generated.write(recieved_file)
-        f.close()
-        response = ":)"
+        image_generated.write(recieved_file_content)
+        file_recieved.close()
         rf.close()
-        print("Terminado")
+        image_generated.close()
+        with open("server-image-rtv.png", "rb") as image_decrypted:
+            client_socket.sendfile(image_decrypted)
+        client_socket.shutdown(socket.SHUT_WR)
         client_socket.close()
+        image_generated.close()
+
+
 main()

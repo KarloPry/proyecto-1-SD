53d67734021d575b6c5bc706634c8c8af73b0c38
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-09-07 11:55:55 -0600

    feat: Comentarios sobre el código

--- client.py
+++ client.py
@@ -5,22 +5,22 @@ llave = 22
 host = '127.0.0.1'
 port = 12345
 original_image = "original_image.png"
-encrypted_image_path = Encryption(original_image).encrypt_image(llave)
-with open(encrypted_image_path, "rb") as encrypted_image:
+encrypted_image_path = Encryption(original_image).encrypt_image(llave) # Encripta la imagen en un archivo nuevo
+with open(encrypted_image_path, "rb") as encrypted_image: # Lee la imagen encriptada
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
-    client_socket.sendfile(encrypted_image)
+    client_socket.sendfile(encrypted_image) # Envia la imagen encriptada
     client_socket.shutdown(socket.SHUT_WR)
     with open("retrieved_image.png", "wb") as retrieved_image:
-        data = client_socket.recv(1024)
+        data = client_socket.recv(1024) # Recibe los datos de la imagen mientras existan
         while data:
             print(f"Recibiendo...")
-            retrieved_image.write(data)
+            retrieved_image.write(data) # Escribe los datos recibidos de la imagen desencriptada
             data = client_socket.recv(1024)
     client_socket.close()
-with open(original_image, "rb") as original_image_file:
+with open(original_image, "rb") as original_image_file: # Lee
     with open("retrieved_image.png", "rb") as retrieved_image_file:
-        if original_image_file.read() == retrieved_image_file.read():
+        if original_image_file.read() == retrieved_image_file.read(): # Compara si son iguales las imagenes
             print("Se regresó la misma imagen")
         else:
             print("La imagen no es la misma")
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 llave = 22
-host = '127.0.0.1'
+host = '148.220.209.32'
 port = 12345
 
 def main():
@@ -11,29 +11,29 @@ def main():
     server_socket.bind((host, port))
     server_socket.listen(1)
     print("Esperando mensajes entrantes")
-    file_recieved = open("server-image-rcv.png", "wb")
     while True:
+        file_recieved = open("server-image-rcv.png", "wb")
         client_socket, client_address = server_socket.accept()
         print("Conexion establecida desde:", client_address)
         data = client_socket.recv(1024)
         while data:
-            print(f"Recibiendo...")
+            print(f"Recibiendo...") #Recibe los datos de la imagen mientras sigan existiendo
             file_recieved.write(data)
             data = client_socket.recv(1024)
-        file_recieved.write(data)
+        file_recieved.write(data) # Escribe los datos de la imagen encriptada en un archivo
         file_recieved.close()
         rf = open("server-image-rcv.png", "rb")
-        recieved_file_content = rf.read()
+        recieved_file_content = rf.read() # Lee los contenidos del la imagen encriptada
         recieved_file_content = bytearray(recieved_file_content)
         for index, value in enumerate(recieved_file_content):
-            recieved_file_content[index] = value ^ llave
+            recieved_file_content[index] = value ^ llave # Desencripta la imagen
         image_generated = open("server-image-rtv.png", "wb")
-        image_generated.write(recieved_file_content)
+        image_generated.write(recieved_file_content) # Escribe la imagen desencriptada
         file_recieved.close()
         rf.close()
         image_generated.close()
         with open("server-image-rtv.png", "rb") as image_decrypted:
-            client_socket.sendfile(image_decrypted)
+            client_socket.sendfile(image_decrypted) # Envia la imagen desencriptada de vuelta
         client_socket.shutdown(socket.SHUT_WR)
         client_socket.close()
         image_generated.close()
